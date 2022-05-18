73ac3566933a1f8cb89ea30e671c7e91c383c242
Ubuntu <ubuntu@ip-172-31-42-10.ap-northeast-2.compute.internal>
2022-05-18 10:48:18 +0000

    toffoli gate and test

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
 from qiskit import *
 
-qreg_q = QuantumRegister(3, 'q')
-creg_c = ClassicalRegister(3, 'c')
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(2, 'c')
 circuit = QuantumCircuit(qreg_q, creg_c)
 
-circuit.reset(qreg_q[0])
-circuit.reset(qreg_q[1])
-circuit.reset(qreg_q[2])
-circuit.h(qreg_q[2])
-circuit.cx(qreg_q[1], qreg_q[2])
-circuit.tdg(qreg_q[2])
-circuit.cx(qreg_q[0], qreg_q[2])
-circuit.t(qreg_q[2])
-circuit.cx(qreg_q[1], qreg_q[2])
-circuit.tdg(qreg_q[2])
-circuit.cx(qreg_q[0], qreg_q[2])
-circuit.t(qreg_q[1])
-circuit.t(qreg_q[2])
+circuit.h(qreg_q[0])
 circuit.cx(qreg_q[0], qreg_q[1])
-circuit.h(qreg_q[2])
-circuit.t(qreg_q[0])
-circuit.tdg(qreg_q[1])
-circuit.cx(qreg_q[0], qreg_q[1])
-circuit.measure(qreg_q,creg_c)
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
 print(circuit)
+
 # Using Qiskit Aer's Qasm Simulator
 simulator = BasicAer.get_backend('qasm_simulator')
 
---
+++ toffoli_gate.py
@@ -0,0 +1,36 @@
+from qiskit import *
+
+qreg_q = QuantumRegister(3, 'q')
+creg_c = ClassicalRegister(3, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.reset(qreg_q[0])
+circuit.reset(qreg_q[1])
+circuit.reset(qreg_q[2])
+circuit.h(qreg_q[2])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.tdg(qreg_q[2])
+circuit.cx(qreg_q[0], qreg_q[2])
+circuit.t(qreg_q[2])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.tdg(qreg_q[2])
+circuit.cx(qreg_q[0], qreg_q[2])
+circuit.t(qreg_q[1])
+circuit.t(qreg_q[2])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.h(qreg_q[2])
+circuit.t(qreg_q[0])
+circuit.tdg(qreg_q[1])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.measure(qreg_q,creg_c)
+print(circuit)
+# Using Qiskit Aer's Qasm Simulator
+simulator = BasicAer.get_backend('qasm_simulator')
+
+# Simulating the circuit using the simulator to get the result
+job = execute(circuit, simulator)
+result = job.result()
+
+# Getting the aggregated binary outcomes of the circuit.
+counts = result.get_counts(circuit)
+print (counts)
