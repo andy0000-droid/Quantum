6649c4978704b2b31f35160aa3ece5bf44e6f2f1
Ubuntu <ubuntu@ip-172-31-42-10.ap-northeast-2.compute.internal>
2022-05-25 11:22:20 +0000

    print sorted result

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from qiskit import *
 
-qreg_q = QuantumRegister(2, 'q')
-creg_c = ClassicalRegister(2, 'c')
+q_circuit = 2
+c_circuit = q_circuit
+
+qreg_q = QuantumRegister(q_circuit, 'q')
+creg_c = ClassicalRegister(c_circuit, 'c')
 circuit = QuantumCircuit(qreg_q, creg_c)
 
 circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
 circuit.cx(qreg_q[0], qreg_q[1])
 circuit.measure(qreg_q[0], creg_c[0])
 circuit.measure(qreg_q[1], creg_c[1])
@@ -20,3 +24,11 @@ result = job.result()
 # Getting the aggregated binary outcomes of the circuit.
 counts = result.get_counts(circuit)
 print (counts)
+
+for i in range(pow(2,q_circuit)):
+    print(str(format(i,'b')).zfill(2),counts[str(format(i,'b')).zfill(2)])
+
+print("\n")
+
+for i in range(pow(2,q_circuit)):
+    print(str(format(i,'b')).zfill(2), counts[str(format(i,'b')).zfill(2)]-(pow(2,10)/pow(2,q_circuit)))
